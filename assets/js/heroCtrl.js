bb22fcb18ed9fc5e171f5de9a1f268e086ac0e6d
David Dronsfield <david.dronsfield@bulkpowders.com>
2017-06-08 17:10:47 +0100

    optimize scripts

--- assets/js/heroCtrl.js
+++ assets/js/heroCtrl.js
@@ -1,12 +1,18 @@
-const Heroslides = $('.hero__slides')
-const HeroSlide = $('.hero__slide')
+var Hero = (function() {
+    const Heroslides = $('.hero__slides')
+	return {
+    	slideCtrl: function() {
+            setInterval(function() {
+                let activeHeroSlide = Heroslides.children('.active')
+                activeHeroSlide.removeClass('active')
+                activeHeroSlide.next().length ? activeHeroSlide.next().addClass('active') : Heroslides.children().first().addClass('active')
+            }, 5000);
+		}
+	};
+})();
 
 $(document).ready(function() {
-	
-	setInterval(function() {
-		let activeHeroSlide = Heroslides.children('.active')
-		activeHeroSlide.removeClass('active')
-		activeHeroSlide.next().length ? activeHeroSlide.next().addClass('active') : Heroslides.children().first().addClass('active')
-	}, 5000)
 
-})
+	Hero.slideCtrl();
+
+});
